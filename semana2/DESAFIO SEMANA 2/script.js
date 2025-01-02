3385af658abbc70256cb979a1d56a98f90b81061
renzo <renzo.paludeto1@gmail.com>
2025-01-02 19:09:06 -0300

    desafio semana 2

--- semana2/DESAFIO SEMANA 2/script.js
+++ semana2/DESAFIO SEMANA 2/script.js
@@ -1,56 +1,12 @@
-function GerarTarefa(){
-    let TarefaNome = document.getElementById('Tarefa').value
-    let TarefaPrioridade = document.getElementById('Prioridade').value
-    let TarefaInicio = document.getElementById('Inicio').value
-    let TarefaConclusao = document.getElementById('Conclusao').value
 
-    if( !TarefaConclusao || !TarefaInicio || !TarefaNome || !TarefaPrioridade) {
-        document.getElementById('output').innerHTML = 'erro'
-        output.style.color = 'red'
-    }else {
-        document.getElementById('output').innerHTML = ''
-        output.style.color = ''
-    
-    let PrazoMS = new Date(TarefaConclusao) - new Date(TarefaInicio)
-    let Prazo = Math.ceil(PrazoMS / (1000 * 60 * 60 * 24))
 
-    let lista = document.createElement('li')
-    let Tarefa = {
-        nome: TarefaNome,
-        prioridade: TarefaPrioridade,
-        inicio: TarefaInicio,
-        conclusao: TarefaConclusao,
-        prazo: Prazo
-    }
-    
-    lista.innerHTML = `Tarefa: ${Tarefa.nome}, Prioridade: ${Tarefa.prioridade}, Prazo: ${Tarefa.prazo}`
+function Adicionar(){
+    let texto = document.getElementById('ToDoTEXTO').value
+    //const CriarLi = document.createElement('li')
+    //const position = document.getElementById('Lista')
+    //position.appendChild(CriarLi)
+    //Lista.appendChild(CriarLi)
 
-    let botaoExcluir = document.createElement('button');
-    botaoExcluir.textContent = "Excluir";
-    botaoExcluir.style.marginLeft = "10px";
-    botaoExcluir.onclick = function () {
-        lista.remove();
-    }
-
-    let botaoEditar = document.createElement('button');
-    botaoEditar.textContent = "Editar";
-    botaoEditar.style.marginLeft = "10px";
-    botaoEditar.onclick = function () {
-        document.getElementById('Tarefa').value = TarefaNome
-        document.getElementById('Prioridade').value = TarefaPrioridade
-        document.getElementById('Inicio').value = TarefaInicio
-        document.getElementById('Conclusao').value = TarefaConclusao
-        lista.remove()
-    }
-
-    lista.appendChild(botaoExcluir)
-    lista.appendChild(botaoEditar)
-    document.getElementById('Lista').appendChild(lista)
+    document.getElementById('Alerta').innerHTML = 'Sucesso'
+    document.getElementById('Lista').innerHTML = `${texto}`
 }
-
-    document.getElementById('Tarefa').value = ''
-    document.getElementById('Prioridade').value = ''
-    document.getElementById('Inicio').value = ''
-    document.getElementById('Conclusao').value = ''
-}
-
